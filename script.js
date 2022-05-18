2dc4a1a0e59c0dc3010767273ebc5b338b0af919
cpt-westphalen <rbwestphalen@gmail.com>
2022-05-17 22:50:21 -0300

    Testing new logic using ES6 classes for users and messages. Not ready yet.

--- script.js
+++ script.js
@@ -1,30 +1,83 @@
-class Usuarios {
-	constructor(nome) {
-		this.nome = nome;
+//objeto-mestre "Users"
+const Users = { 
+	id: {}, //this is where every new User() is stored. Access it thru Users.id[name]
+	add(name, color) {
+		//this function creates a new User() and stores it into id[name].
+		Users.id[name] = Object.create(User);
+		Users.id[name].username = name;
+		Users.id[name].color = color;
+		console.log('Novo usuário criado com sucesso!');
+		console.log(Users);
+	},
+};
+
+//modelo de usuário,
+const User = {
+	username: '',
+	color: '',
+	messages: [],
+	send(texto) {
+		let i = this.messages.length;
+		this.messages[i] = ["yyyy-mm-dd-hh-mm", this.username, texto]; //logs message on user
+		console.log(this, this.messages[i]);
+		messageDisplay(this.username, this.color, texto); //sends info to uptade DOM
 	}
- //inserir pesquisador de msg por usuario
+};
+
+function getText() {
+	let user = document.getElementById('select-user').value; // -> falta o seletor
+	let text = document.getElementById('message-input-field').value;
+	console.log('tentando enviar mensagem');
+	console.log(Users.id[user]);
+	Users.id[user].send(text);
 }
 
-var usernames = []; //deve ter um jeito melhor de unir users
+
+function messageDisplay(usernameDisplay, usernameColor, textFieldInput) {
+	let newPost = document.createElement('div');
+	newPost.classList.add('chat-post');
+	//if (esse post é seu) { faça ele também ser class yourText };
+	let newH3 = document.createElement('h3');
+	newH3.classList.add('username-display');
+	newH3.textContent = '@' + usernameDisplay;
+	newH3.style.color = Users.id[usernameDisplay].color;
+	let newPara = document.createElement('p');
+	newPara.classList.add('chat-message');
+	newPara.textContent = textFieldInput;
+	newPost.append(newH3, newPara);
+	chatLog.prepend(newPost);
+}
+
 var createdUser = false; //só pra que não seja possível enviar mensagens antes de ter um usuário (e talvez 'liberar' o chat só depois)
-var makeUserButton = document.getElementById("usernameInputButton");
-var chatboxLog = document.getElementById("chat-messages"); // main div.
+var chatLog = document.getElementById("chat-log");
 
 console.log("javascript iniciado com sucesso");
 
-//Função base para click e Enter ao digitar o nome;
+//Função base para click, falta Enter ao digitar o name;
+// function criadorDeUser(){
+// 	let usernameInput = document.getElementById("usernameInputText").value;
+// 	document.getElementById("usernameInputText").value = "";
+// 	let usernameColor = document.getElementById("usernameInputColor").value;
+// 	Users.add(usernameInput, usernameColor);
+// 	createdUser = true; //trigger pra liberar chatbox
+// 	console.log("novo objeto de usuario criado");
+// 	console.log(Users);
+// 	document.getElementById("username-display").innerText = String("Novo usuário criado com sucesso: " + Users.id[usernameInput].username);
+// }
+
 function criadorDeUser(){
-	let usernameInput = document.getElementById("usernameInputText").value;
-	document.getElementById("usernameInputText").value = "";
-	let nextUsernameIndex = usernames.length;
-	usernames[nextUsernameIndex] = new Usuarios(usernameInput); //aqui vão os parâmetros depois
-	createdUser = true; //trigger pra liberar chatbox
-	console.log("novo objeto de usuario criado");
-	console.log(usernames[nextUsernameIndex].nome, nextUsernameIndex);
-	document.getElementById("usernameDisplay").innerText = "Novo usuário criado com sucesso: @" + usernames[nextUsernameIndex].nome;
+	let usernameInput = document.getElementById("username-input-text").value;
+	document.getElementById("username-input-text").value = "";
+	let usernameColor = document.getElementById("username-input-color").value;
+	Users.add(usernameInput, usernameColor);
+	document.getElementById("username-display").innerText = String("Novo usuário criado com sucesso: " + Users.id[usernameInput].username);
 }
 
-makeUserButton.addEventListener('click', criadorDeUser);
+function addUserOnSelector(usernameInput){
+	//chamar para adicionar username no seletor e ter valor: usernameInput;
+}
 
 //fazer o scroll começar embaixo
-window.onload = function() { chatboxLog.scrollTop = chatboxLog.scrollHeight; }
+window.onload = function() { chatLog.scrollTop = chatLog.scrollHeight; }
+
+//Falta fazer o selector funcionar e então adicionar a função ao click do Enviar
